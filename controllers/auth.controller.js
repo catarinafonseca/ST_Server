d9e816fcba5705f41c99f1f44cb8b34211f2dc80
bms14 <brunoires14@gmail.com>
2021-05-20 16:09:49 +0100

    ok

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,5 +2,5 @@ const jwt= require("jsonwebtoken");
 const bcrypt= require("bcryptjs");
 
 const config = require("../config/auth.config.js");
-const db= require("../models");
+const sql = require("./db.js"); 
 const User = db.user; 
